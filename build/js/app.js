9d0b6a55058ef4115ddb4c3da073666375b7b700
Serena Young <serenaf479@gmail.com>
2017-05-04 23:19:35 -0700

    Adding new profile SVG. Testing SVG animation

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,9 @@
-console.log('app');
+var profile = Snap.select("#profile");
+console.log(profile);
+profile.hover(
+    function() {
+        profile.attr({fill: 'red'});
+    },
+    function() {
+        console.log('out');
+    });
