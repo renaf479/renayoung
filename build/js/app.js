8a02ecf170f1559b98ea950533f55cf39a41129f
Serena Young <serenaf479@gmail.com>
2017-05-06 00:05:54 -0700

    New profile icon (again). Updated button styles w/ui effects

--- build/js/app.js
+++
@@ -1,9 +0,0 @@
-var profile = Snap.select("#profile");
-console.log(profile);
-profile.hover(
-    function() {
-        profile.attr({fill: 'red'});
-    },
-    function() {
-        console.log('out');
-    });
