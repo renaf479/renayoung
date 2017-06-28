dd5b1457ceb20363638f7800fa7fc991678920ae
Serena Young <serenaf479@gmail.com>
2017-06-27 22:02:05 -0700

    (EOD) Computer issues - committing progress

--- content/works/ibm.md
+++ content/works/ibm.md
@@ -5,6 +5,8 @@ type: companies
 weight: 1
 year: June.2015 - Present
 position: Software Engineer
+imagesLeft: ["http://via.placeholder.com/100x400", "http://via.placeholder.com/200x400"]
+imagesRight: []
 ---
 
 Frontend developer on IBM Watson Campaign Automation - a cloud based digital marketing platform. I work with distributed teams using AngularJS and internal IBM components for frontend, and backend development done in Java. I also implement test coverage in Jasmine and automation tests through Fitnesse
--- static-src/assets/src-js/app.js
+++ static-src/assets/src-js/app.js
@@ -1,29 +1,3 @@
-"use strict";
+import iconProfile from './svg/profile';
 
-function openDialog(e) {
-    let articleId = '#' + e.target.ownerSVGElement.dataset.slug;
-    Avgrund.show(articleId);
-}
-
-
-const worksWrapper = document.querySelector(".works");
-let workNodes = worksWrapper.getElementsByClassName('timeline__item__svg');
-
-Array.from(workNodes).forEach((workItem) => {
-    workItem.addEventListener('click', openDialog);
-});
-
-/*
-
-var wrapper = document.querySelector(".wrapper");
-
-var divs = wrapper.getElementsByTagName("div");
-
-for (i = 0; i < divs.length; ++i) {
-    each = divs[i];
-    if (each.classList.contains("test2")) {
-        each.style.display = "none";
-    }
-}
-*/
-
+const profile = iconProfile;
